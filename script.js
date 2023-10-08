256d026672fe623cf9b132d244248e01ab707fba
Denis Zudilin <zudilin2014@yandex.ru>
2023-10-08 15:26:56 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-const title = 1;
-const screens = 2;
-const screenPrice = 3;
-const rollback = 4;
-const fullPrice = 5;
-const adaptive = 6;
+const title = "Project";
+const screens = "Simple, Complex, Interactive";
+const screenPrice = 50;
+const rollback = 80;
+const fullPrice = 30;
+const adaptive = true;
+
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(screenPrice);
+console.log(fullPrice);
+console.log(screens.toLowerCase().split(", "));
+console.log(30 * (80 / 100));
