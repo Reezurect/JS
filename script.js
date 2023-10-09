59fc4b9694ffda7b1a07e0e0447b6865e7eda643
Denis Zudilin <zudilin2014@yandex.ru>
2023-10-09 20:52:58 +0300

    lesson03

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const title = "Project";
-const screens = "Simple, Complex, Interactive";
-const screenPrice = 50;
-const rollback = 80;
-const fullPrice = 30;
-const adaptive = true;
+'use strict';
+
+const title = prompt("Как называется ваш проект?");
+const screens = prompt("Какие типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+const screenPrice = prompt("Сколько будет стоить данная работа?", "12000");
+const rollback = 12;
+const adaptive = confirm("Нужен ли адаптив на сайте?");
+const service1 = prompt("Какой дополнительный тип услуг нужен?");
+const servicePrice1 = prompt("Сколько это будет стоить?");
+const service2 = prompt("Какой дополнительный тип услуг нужен?");
+const servicePrice2 = prompt("Сколько это будет стоить?");
+const fullPrice = screenPrice + servicePrice1 + servicePrice2;
+const servicePercentPrice = (fullPrice * (12 / 100));
 
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(screenPrice);
-console.log(fullPrice);
-console.log(screens.toLowerCase().split(", "));
-console.log(30 * (80 / 100));
+switch (true) {
+    case fullPrice > 30000: alert("Даем скидку в 10%");
+        break;
+    case fullPrice > 15000 && fullPrice < 30000: alert("Даем скидку в 5%");
+        break;
+    case fullPrice < 15000 && fullPrice > 0: alert("Скидка не предусмотрена");
+    default: alert("Что то пошло не так");
+};
+
+
+console.log(title);
+console.log(screens);
+console.log(service1);
+console.log(servicePrice1);
+console.log(service2);
+console.log(servicePrice2);
+console.log("Откат посреднику", Math.floor(servicePercentPrice));
