e5add042569749ec42fccfdf5ea713d2b5023654
Denis Zudilin <zudilin2014@yandex.ru>
2023-10-20 22:15:04 +0300

    lesson10

--- script.js
+++ script.js
@@ -1,26 +1,65 @@
 'use strict';
 
-const title = document.getElementsByTagName('h1')[0];
-const butCalculate = document.getElementsByClassName("handler_btn");
-const butPlus = document.querySelector(".screen-btn");
-const otherItems1 = document.querySelectorAll(".other-items.percent");
-const otherItems2 = document.querySelectorAll(".other-items.number");
-const inputRange = document.querySelector(".rollback input[type = 'range']");
-const spanRange = document.querySelector(".rollback span[class = 'range-value']");
-const totalInput = function () {
-    let list = document.getElementsByClassName("total-input");
-    for (let i = 0; i < list.length; i++) {
-        console.log(list[i]);
+const books = document.querySelectorAll('.books'),
+    arrBooks = document.querySelectorAll('.book');
+const arr = Object.keys(arrBooks).sort((prev, next) => {
+    if (arrBooks[prev].firstElementChild.innerText > arrBooks[next].firstElementChild.innerText) {
+        return 1;
     }
+    if (arrBooks[prev].firstElementChild.innerText < arrBooks[next].firstElementChild.innerText) {
+        return -1;
+    }
+});
+for (let i = 0; i < arr.length; i++) {
+    books[0].appendChild(arrBooks[arr[i]]);
 };
-let listScreen = document.querySelectorAll(".screen");
 
-console.log(title);
-console.log(butCalculate);
-console.log(butPlus);
-console.log(otherItems1);
-console.log(otherItems2);
-console.log(inputRange);
-console.log(spanRange);
-console.log(totalInput());
-console.log(listScreen);
+document.querySelector('body').style.backgroundImage = 'url(./image/Backgrounds.jpg)';
+
+books[0].children[2].querySelector('h2').querySelector('a').text = 'Книга 3. this и Прототипы Объектов';
+
+document.querySelector('.adv').remove();
+
+const sortChapter = collection => {
+    const sortElements = arr => {
+        const arrInd = Object.keys(arr).sort((prev, next) => {
+            if (arr[prev].textContent > arr[next].textContent) {
+                return 1;
+            }
+            if (arr[prev].textContent < arr[next].textContent) {
+                return -1;
+            }
+        });
+        let arrNew = [];
+        for (let i = 0; i < arrInd.length; i++) {
+            arrNew.push(arr[arrInd[i]]);
+        }
+        return arrNew;
+    };
+
+    const elem = collection.querySelectorAll('li');
+    let arrChapter = [],
+        arrApp = [];
+    elem.forEach(el => {
+        if (el.textContent.indexOf('Введение') > -1) {
+            collection.insertBefore(el, elem[0]);
+        }
+        if (el.textContent.indexOf('Предисловие') > -1) {
+            collection.insertBefore(el, elem[1]);
+        }
+        if (el.textContent.indexOf('Глава') > -1) { arrChapter.push(el); }
+        if (el.textContent.indexOf('Приложение') > -1) { arrApp.push(el); }
+    });
+    arrChapter = sortElements(arrChapter);
+    arrChapter.forEach(el => { collection.appendChild(el); });
+    arrApp = sortElements(arrApp);
+    arrApp.forEach(el => { collection.appendChild(el); });
+};
+
+sortChapter(books[0].children[1].querySelector('ul'));
+sortChapter(books[0].children[4].querySelector('ul'));
+
+let child = document.createElement('li');
+child.innerText = 'Глава 8: За пределами ES6';
+books[0].children[5].querySelector('ul').appendChild(child);
+sortChapter(books[0].children[5].querySelector('ul'));
